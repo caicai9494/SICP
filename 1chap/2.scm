c56f666d40d19b77ac7066a084f677170477fc72
linzhe <cailinzhe@gmail.com>
2015-04-04 00:24:45 -0400

    more

--- 1chap/2.scm
+++ 1chap/2.scm
@@ -91,5 +91,158 @@
       (+ (pascal (- height 1) (- pos 1)) (pascal (- height 1) pos))
       )))
 
-(pascal 40 25)
+(pascal 40 1)
+
+(define (sine ang)
+  (define (cube x)
+    (* x x x))
+  (define (p x)
+    (- (* 3 x) (* 4 (cube x))))
+  (if (< ang 0.01)
+    ang
+    (p (sine(/ ang 3.0)))))
+
+(sine 1)
+
+(define (fast-pow  base power)
+  (cond
+    ((= power 0) 1)
+    ;((= power 1) base)
+    ((even? power) (fast-pow (* base base) (/ power 2)))
+    (else (* base (fast-pow base (- power 1))))
+    ))
+(fast-pow 2 10)
+
+(define (fast-mul lhs rhs)
+  (cond 
+    ((or (= lhs 0) (= rhs 0)) 0)
+    ((even? rhs) (fast-mul (* 2 lhs) (/ rhs 2)))
+    (else (+ lhs (fast-mul lhs (- rhs 1))))))
+
+(fast-mul 5 40)
+
+(define (mul-iter lhs rhs)
+  (cond 
+    ((or (= lhs 0) (= rhs 0)) 0)
+    (else (+ lhs (mul-iter lhs (- rhs 1))))))
+
+(mul-iter 5 40)
+
+(define (fast-mul-i lhs rhs)
+(define (fast-mul-iter lhs rhs holder)
+  (cond 
+    ((= rhs 1) holder)
+    ((or (= lhs 0) (= rhs 0)) 0)
+    ((even? rhs) (fast-mul-iter lhs (/ rhs 2) (* 2 lhs)))
+    (else (fast-mul-iter lhs (- rhs 1) (+ lhs holder)))))
+  (fast-mul-iter lhs rhs 0)
+)
+
+(fast-mul-i 5 20)
+
+
+(define (prime? n)
+  (define (prime-inner n testn)
+    (cond 
+      ((or (= n 2) (> testn (sqrt n))) #t)
+      ((= 0 (remainder n testn)) #f)
+      (else (prime-inner n (+ 1 testn)))))
+  (prime-inner n 2))
+
+(prime? 3)
+(prime? 4)
+(prime? 17)
+(prime? 18)
+
+(define (gcd a b)
+  (if (= b 0) 
+    a
+    (gcd b (remainder a b))))
+(gcd 206 40)
+
+(define (expmod base power rmd)
+  (cond
+    ((= power 0) 1)
+    ((even? power) 
+     (remainder (expmod (* base base) (/ power 2) rmd) rmd))
+    (else (remainder (* base (expmod base (- power 1) rmd)) rmd))
+    ))
+
+(expmod 2 5 4)
+
+(define (fermat-test n)
+  (define (try-it r)
+    (= (expmod r n n) r))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (quick-prime? n)
+  (define (inner n count)
+    (cond
+      ((= count 0) #t)
+      ((fermat-test n) (inner n (- count 1)))
+      (else #f)))
+  (inner n 1)
+  )
+
+(quick-prime? 100)
+
+(gcd 19999 1999)
+
+(define (timed-prime-test n)
+  (define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time)
+    )
+  (define (start-prime-test n start-time)
+    (if (quick-prime? n)
+      (report-prime (- (runtime) start-time))))
+  (newline)
+  (display n)
+  (start-prime-test n (runtime))
+  )
+
+(timed-prime-test 53)
+
+(define (prime_start-from n count)
+  (define (time-prime-start-from n count start-time)
+    (cond
+      ((= count 0) (- (runtime) start-time))
+      ((quick-prime? n) (newline) (display n) (time-prime-start-from (+ 2 n) (- count 1) start-time)) 
+      (else (time-prime-start-from (+ 2 n) count start-time))))
+  (time-prime-start-from n count (runtime)))
+
+
+(prime_start-from 1001 3)
+;(prime_start-from 10001 3)
+;(prime_start-from 100001 3)
+
+(define (small-divisor n)
+  (define (find-divisor n test-n)
+    (cond 
+      ((> (* test-n test-n) n) n)
+      ((= (remainder n test-n) 0) test-n)
+      (else (find-divisor n (+ 1 test-n)))))
+  (find-divisor n 2)
+  )
+
+(define (small-divisor-rev n)
+  (define (next n) (+ n 2))
+  (define (find-divisor n test-n)
+    (cond 
+      ((> (* test-n test-n) n) n)
+      ((= (remainder n test-n) 0) test-n)
+      (else (find-divisor n (next test-n)))))
+  (find-divisor n 2)
+  )
+
+(define (timing func para)
+  (define (timing-inner func start-time)
+    (func)
+    (display (- (runtime) start-time)))
+  (timing-inner func (runtime)))
+
+
+(timing (small-divisor-rev 6))
+
+
 
